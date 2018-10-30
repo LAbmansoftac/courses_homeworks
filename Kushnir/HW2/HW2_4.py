0b7a6325630e8a577da6e6bdcd5bb36f3c5de4ae
labmansoftac@gmail.com <labmansoftac@gmail.com>
2018-10-30 13:15:01 +0200

    Restarting git story

--- Kushnir/HW2/HW2_4.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Enter number: '))
-i = 1
-
-width = x + (x - 1)
-
-
-for i in range (1, x):
-    print("{0:^{1}.{2}}".format("*" * (i + (i - 1)), width, width))
--- Kushnir/HW3/HW3_2.py
+++
@@ -1,6 +0,0 @@
-f = open('HW3_2.py')
-line = f.readline()
-while line:
-    print (line),
-    line = f.readline()
-f.close()
